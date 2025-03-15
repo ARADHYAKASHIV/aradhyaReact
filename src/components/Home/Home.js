3f8d4ee3753d5136f2178b9a15bf818c159f885d
Aradhya Kashiv <kashiv.aradhya@gmail.com>
2025-03-15 23:20:40 +0530

    Upgrade React and ReactDOM to version 19; comment out unused Spline import in Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Spline from "@splinetool/react-spline";
+// import Spline from "@splinetool/react-spline";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
@@ -34,7 +34,7 @@ function Home() {
             <Col md={5} style={{ paddingBottom: 20 }}>
               <div style={{ width: '100%', height: '450px', overflow: 'hidden' }}>
                 
-                <Spline scene="https://prod.spline.design/k5bbg4epJjkb293I/scene.splinecode" />
+                {/* <Spline scene="https://prod.spline.design/k5bbg4epJjkb293I/scene.splinecode" /> */}
 
               </div>
             </Col>
